045eded23b2b53af8985cd93703558e2275a4324
Lyall Cooper <git@lyall.co>
2026-07-18 16:04:45 +0900

    Fix site dependency vulnerabilities

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
+import { unified } from '@astrojs/markdown-remark';
 import rehypeDocLinks from './src/lib/rehype-doc-links.mjs';
 
 export default defineConfig({
   site: 'https://magritte.lyall.co',
   markdown: {
-    rehypePlugins: [rehypeDocLinks],
+    processor: unified({ rehypePlugins: [rehypeDocLinks] }),
     shikiConfig: {
       themes: { light: 'solarized-light', dark: 'solarized-dark' },
       defaultColor: false,
